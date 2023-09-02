ab68acb786b524551a88518b320735a30237f0cd
Marlon <marlon.lalangui@epn.edu.ec>
2023-09-02 12:39:48 -0500

    ORINGEN FRONT

---
+++ src/config/nodemailer.js
@@ -0,0 +1,16 @@
+// send mail with defined transport object
+const sendMailToRecoveryPassword = async(userMail,token)=>{
+    let info = await transport.sendMail({
+    from: 'admin@vet.com',
+    to: userMail,
+    subject: "Correo para reestablecer tu contraseña",
+    html: `
+    <h1>Sistema de gestión (VET-ESFOT 🐶 😺)</h1>
+    <hr>
+    <a href="https://backend-exam-b2eg.onrender.com/api/recuperar-password${token}">Clic para reestablecer tu contraseña</a>
+    <hr>
+    <footer>Grandote te da la Bienvenida!</footer>
+    `
+    });
+    console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
+}
